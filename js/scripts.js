f8ecc3b1401c7a1bde28cf6d273b8ed6c326f572
ArturWitkowski <awitkowski@aerobit.pl>
2017-02-02 01:38:21 +0100

    zadanie 8.5

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+var a = prompt("Podaj wartość zmiennej a"),
+	b = prompt("Podaj wartość zmiennej b");
+value = (a*a) + (2 * a * b) - (b*b);
+console.log(value);
+
+var result = a > b ? "a jest większe od b" : "b jest większe lub równe a";
+console.log(result);
