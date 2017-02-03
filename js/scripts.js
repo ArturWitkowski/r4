0fdf1df16179c987abe3a77b070107ccdc032f22
ArturWitkowski <awitkowski@aerobit.pl>
2017-02-03 09:47:48 +0100

    Zadanie 8.5 obsluga wyniku = 0

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,14 @@
 var a = prompt("Podaj wartość zmiennej a"),
 	b = prompt("Podaj wartość zmiennej b");
-value = (a*a) + (2 * a * b) - (b*b);
+var value = (a * a) + (2 * a * b) - (b * b);
+
 console.log(value);
 
-var result = a > b ? "a jest większe od b" : "b jest większe lub równe a";
-console.log(result);
+if (value === 0) {
+	console.log("Wynik równy zero");
+} else {
+	var result = (value > 0) ? "Wynik jest większy od zera" : "Wynik jest mniejszy od zera";
+	consoe.log(result);
+}
+
+
